07715197099d07b05c01c96a9a82392f3af2ea75
Tim Ruesink <tim@datachat.ai>
2023-01-17 17:51:59 -0600

    change theme

--- constants/theme.tsx
+++ constants/theme.tsx
@@ -1,21 +1,23 @@
 import { createTheme } from '@mui/material/styles';
-import { blue } from '@mui/material/colors';
+import { yellow, blueGrey, pink } from '@mui/material/colors';
 import { Montserrat } from '@next/font/google';
 import ButtonLink from '../components/Links/ButtonLink';
 
 const mainFont = Montserrat({ subsets: ['latin'] });
 
-const mainColor = blue[800];
+const mainColor = pink;
+const secondaryColor = yellow;
+const textColor = blueGrey;
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: mainColor,
+      main: mainColor[900],
       contrastText: '#ffffff',
     },
     secondary: {
-      main: '#ffffff',
-      contrastText: mainColor,
+      main: secondaryColor[600],
+      contrastText: textColor[900],
     },
   },
   typography: {
