4c2afcd6d3ae7818afe49362e69c20d79e2d7ac1
Tim Ruesink <tim@datachat.ai>
2023-04-15 07:37:13 -0500

    update website colors

--- constants/theme.tsx
+++ constants/theme.tsx
@@ -1,28 +1,31 @@
 import { createTheme } from '@mui/material/styles';
-import { yellow, blueGrey, pink } from '@mui/material/colors';
-import { Montserrat } from '@next/font/google';
+import { blueGrey, deepOrange } from '@mui/material/colors';
+import { Source_Sans_Pro } from '@next/font/google';
 import ButtonLink from '../components/Links/ButtonLink';
 
-const mainFont = Montserrat({ subsets: ['latin'] });
+const mainFont = Source_Sans_Pro({ weight: '400', subsets: ['latin'] });
 
-const mainColor = pink;
-const secondaryColor = yellow;
+const mainColor = blueGrey;
+const secondaryColor = deepOrange;
 const textColor = blueGrey;
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: mainColor[900],
-      light: mainColor[200],
+      main: mainColor[800],
+      light: mainColor[600],
       contrastText: '#ffffff',
     },
     secondary: {
-      main: secondaryColor[600],
+      main: secondaryColor[200],
       contrastText: textColor[900],
     },
   },
   typography: {
     fontFamily: mainFont.style.fontFamily,
+    button: {
+      fontSize: '1rem',
+    },
   },
   components: {
     MuiButtonBase: {
